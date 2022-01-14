94e0c5cf271f6787e5ce5146012aa679bcaf5dcf
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-14 18:07:54 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -6,7 +6,7 @@ VALUES     ('158023', 'https://cdn.sofifa.org/players/4/19/158023.png','Medium/ 
 	   ('193080', 'https://cdn.sofifa.org/players/4/19/193080.png','Medium/ Medium'),'De Gea',	     27, 260 000, 0004, 0004),
 	   ('192985', 'https://cdn.sofifa.org/players/4/19/192985.png','High/ High',	 'K. De Bruyne',     27, 355 000, 0005, 0005);
 
--- Заповнення таблиці Products
+-- Заповнення таблиці Countries
 INSERT INTO Countries
 VALUES 	   (0001, 'Argentina',  'https://cdn.sofifa.org/flags/52.png'),
 	   (0002, 'Portugal',   'https://cdn.sofifa.org/flags/38.png'),
