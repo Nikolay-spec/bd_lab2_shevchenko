797237bb4cea0b1410f2a857b0da312b26c81182
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-14 17:43:57 +0200

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,57 @@
+-- Заповнення таблиці Players
+INSERT INTO Players
+VALUES ('11111', 'SomeURL1','Low/Low',		 'Lower',	  21,111111, 0001, 00000002),
+	   ('11112', 'SomeURL2','Low/Medium',	 'Justplayer',22,222222, 0002, 00000004),	
+	   ('11113', 'SomeURL3','Medium/Medium', 'Normal',	  23,333333, 0003, 00000006),
+	   ('11114', 'SomeURL4','Medium/High'),	 'Fineguy',	  24,444444, 0004, 00000008),
+	   ('11115', 'SomeURL5','High/High',	 'Coolguy',	  25,555555, 0005, 00000010);
+
+-- Заповнення таблиці Products
+INSERT INTO Countries
+VALUES (0001, 'SomeWhere1','SomeName1'),
+	   (0002, 'SomeWhere2','SomeName2'),
+	   (0003, 'SomeWhere3','SomeName3'),
+	   (0004, 'SomeWhere4','SomeName4'),
+	   (0005, 'SomeWhere5','SomeName5'),
+	   (0006, 'SomeWhere6','SomeName6'),
+	   (0007, 'SomeWhere7','SomeName7'),
+	   (0008, 'SomeWhere8','SomeName8'),
+	   (0009, 'SomeWhere9','SomeName9'),
+	   (0010, 'SomeWhere10','SomeName10')
+	   ;
+
+-- Заповнення таблиці Clubs
+INSERT INTO Clubs
+VALUES (00000001, 'SomeIMG1', 'Guys1'),
+	   (00000002, 'SomeIMG2', 'Guys2'),
+	   (00000003, 'SomeIMG3', 'Guys3'),
+	   (00000004, 'SomeIMG4', 'Guys4'),
+	   (00000005, 'SomeIMG5', 'Guys5'),
+	   (00000006, 'SomeIMG6', 'Guys6'),
+	   (00000007, 'SomeIMG7', 'Guys7'),
+	   (00000008, 'SomeIMG8', 'Guys8'),
+	   (00000009, 'SomeIMG9', 'Guys9'),
+	   (00000010, 'SomeIMG10','Guys10');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
