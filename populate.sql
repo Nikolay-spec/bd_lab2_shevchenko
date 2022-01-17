76461806e0a9193a6e0bbbd9b3a888ef56e887cc
Nikolay-spec <55550441+Nikolay-spec@users.noreply.github.com>
2022-01-17 02:01:54 +0200

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,14 +1,7 @@
--- Заповнення таблиці Players
-INSERT INTO Players
-VALUES     ('158023', 'https://cdn.sofifa.org/players/4/19/158023.png','Medium/ Medium', 'L. Messi',	     31, 565 000, 0001, 0001),
-	   ('20801',  'https://cdn.sofifa.org/players/4/19/20801.png', 'High/ Low',	 'Cristiano Ronaldo',33, 405 000, 0002, 0002),	
-	   ('190871', 'https://cdn.sofifa.org/players/4/19/190871.png','High/ Medium',   'Neymar Jr',	     26, 290 000, 0003, 0003),
-	   ('193080', 'https://cdn.sofifa.org/players/4/19/193080.png','Medium/ Medium'),'De Gea',	     27, 260 000, 0004, 0004),
-	   ('192985', 'https://cdn.sofifa.org/players/4/19/192985.png','High/ High',	 'K. De Bruyne',     27, 355 000, 0005, 0005);
 
 -- Заповнення таблиці Countries
 INSERT INTO Countries
-VALUES 	   (0001, 'Argentina',  'https://cdn.sofifa.org/flags/52.png'),
+VALUES (0001, 'Argentina',  'https://cdn.sofifa.org/flags/52.png'),
 	   (0002, 'Portugal',   'https://cdn.sofifa.org/flags/38.png'),
 	   (0003, 'Brazil',	'https://cdn.sofifa.org/flags/54.png'),
 	   (0004, 'Spain',	'https://cdn.sofifa.org/flags/45.png'),
@@ -17,14 +10,19 @@ VALUES 	   (0001, 'Argentina',  'https://cdn.sofifa.org/flags/52.png'),
 
 -- Заповнення таблиці Clubs
 INSERT INTO Clubs
-VALUES 	   (0001, 'https://cdn.sofifa.org/teams/2/light/241.png', 'FC Barcelona'),
+VALUES (0001, 'https://cdn.sofifa.org/teams/2/light/241.png', 'FC Barcelona'),
 	   (0002, 'https://cdn.sofifa.org/teams/2/light/45.png', 'Juventus'),
 	   (0003, 'https://cdn.sofifa.org/teams/2/light/73.png', 'Paris Saint-Germain'),
 	   (0004, 'https://cdn.sofifa.org/teams/2/light/11.png', 'Manchester United'),
 	   (0005, 'https://cdn.sofifa.org/teams/2/light/10.png', 'Manchester City');
 
-
-
+-- Заповнення таблиці Players
+INSERT INTO Players
+VALUES (158023, 'https://cdn.sofifa.org/players/4/19/158023.png','Medium/ Medium', 'L. Messi',	     31, 565000, 0001, 0001),
+	   (20801,  'https://cdn.sofifa.org/players/4/19/20801.png', 'High/ Low',	 'Cristiano Ronaldo',33, 405000, 0002, 0002),	
+	   (190871, 'https://cdn.sofifa.org/players/4/19/190871.png','High/ Medium',   'Neymar Jr',	     26, 290000, 0003, 0003),
+	   (193080, 'https://cdn.sofifa.org/players/4/19/193080.png','Medium/ Medium','DeGea',	     27, 260000, 0004, 0004),
+	   (192985, 'https://cdn.sofifa.org/players/4/19/192985.png','High/ High',	 'K. De Bruyne',     27, 355000, 0005, 0005);
 
 
 
